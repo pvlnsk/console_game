5d6136d7a09cde5b1fc7c85f6a2f735cd0b6c62b
Pavel Lygin <p.lygin@cft.ru>
2021-05-22 16:55:01 +0700

    fixed game state print

--- client/src/main/java/ru/cft/fs/game/client/UpdateListener.java
+++ client/src/main/java/ru/cft/fs/game/client/UpdateListener.java
@@ -3,14 +3,28 @@ package ru.cft.fs.game.client;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
+import ru.cft.fs.game.common.ConsolePrinter;
 import ru.cft.fs.game.common.Constants;
 
 @Slf4j
 @Service
 public class UpdateListener {
 
-  @RabbitListener(queues = Constants.QUEUE_NAME, concurrency = "2")
+  private static final String EMPTY_STRING = " ";
+  private static final String GAME_STATE_SEPARATOR =
+      EMPTY_STRING.repeat(15) + "\n" + EMPTY_STRING.repeat(30) + "!!! GAME STATE !!!" + "\n";
+  private static final String GAME_STATE_TEMPLATE =
+      GAME_STATE_SEPARATOR + "%s" + GAME_STATE_SEPARATOR;
+
+  private final ConsolePrinter consolePrinter;
+
+  public UpdateListener(ConsolePrinter consolePrinter) {
+    this.consolePrinter = consolePrinter;
+  }
+
+  @RabbitListener(queues = Constants.QUEUE_NAME)
   public void consumeGameState(String gameStateText) {
-    log.info("game state: {}", gameStateText);
+
+    consolePrinter.print(String.format(GAME_STATE_TEMPLATE, gameStateText));
   }
 }
